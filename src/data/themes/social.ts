7f23624919c46c8944ece65f48bd2439f3bfcb4b
christicheler-rgb <christicheler@gmail.com>
2026-09-25 03:21:21 -0700

    Expand social theme to 15 listed stocks.

--- src/data/themes/social.ts
+++ src/data/themes/social.ts
@@ -1,2 +1,1 @@
-import type { SearchTrend } from "../types";
-export const THEME_SOCIAL: SearchTrend = { id: "social", rank: 3, name: "Social platforms", shortName: "Social", tagline: "t", monthlySearchesM: 1280, yoyChangePct: 4, queries: ["facebook"], spark: [90, 91, 92, 93, 94, 95, 96, 94, 95, 97, 98, 96], thesis: "t", sizeNote: "t", stocks: [] };
+PLACEHOLDER
